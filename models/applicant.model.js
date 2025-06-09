f92d5bb1ce29e18a3283caca9ea00e2098f3f3b6
Ferdinand <dev@erisngraduate.com>
2025-06-09 20:06:02 +0200

    09/06

--- models/applicant.model.js
+++ models/applicant.model.js
@@ -12,6 +12,7 @@ const applicantSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
+  idnumber: { type: Number, required: true, unique: true },
   dob: { type: Date, required: true },
   gender: { type: String, required: true },
   nationality: { type: String, required: true },
